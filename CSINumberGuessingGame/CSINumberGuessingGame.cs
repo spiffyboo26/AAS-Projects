a103808937abc54bf5bba9e5dee6fd0c398bd565
spiffyboo26 <157311076+spiffyboo26@users.noreply.github.com>
2024-01-21 11:51:58 -0700

    Add files via upload

---
+++ CSINumberGuessingGame/CSINumberGuessingGame.cs
@@ -0,0 +1,50 @@
+﻿//Comes from Francesco Magliocco, the man that tells it like it is.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSINumberGuessingGame
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random(); //class to referenct random number
+
+            int winNum = r.Next(1, 100);//generates a winning number between 1-100
+
+            bool win = false;//sets the norm to never win
+
+            do//starts do-while loop
+            {
+                Console.Write("Guess a number between 1 and 100: ");//tells user to pick a number and writes to screen
+                string s = Console.ReadLine()!;//reads input from the user
+
+                int i = int.Parse(s);//makes input an integer
+
+                if (i > winNum)//if else to check if input is higher than winning number
+                {
+                    Console.WriteLine("To high! Guess lower...");//tells user if input is too high, goes to line 42
+                }
+                else if (i < winNum)//if else to check if input is lower than winning number
+                {
+                    Console.WriteLine("To low! Guess lower...");//tells user if input is too low, goes to line 42
+                }
+                else if (i == winNum)//Checks if input is = to winning number
+                {
+                    Console.WriteLine("YOU WIN!");//tells user they won
+                    win = true;//changes win to true instead of false
+                }
+
+                Console.WriteLine();//creates blank line after each if, else if statement
+            } while (win == false);//as long as win is false it continues the loop, once win is true the loop ends
+
+            Console.WriteLine("Thank you for playing the game!");//exit statement to make user friendly
+            Console.Write("Press any key to  finish.");//tells user how to exit the game
+            Console.ReadKey(true);//reads any keystroke and exits the game
+        }
+    }
+}
---
+++ CSINumberGuessingGame/NumberGuessingGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSINumberGuessingGame
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(1, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number between 1 and 100: ");
+                string s = Console.ReadLine()!;
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("To high! Guess lower...");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("To low! Guess lower...");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+            } while (win == false);
+
+            Console.WriteLine("Thank you for playing the game!");
+            Console.Write("Press any key to  finish.");
+            Console.ReadKey(true);
+        }
+    }
+}
